9e0a8817938a8dfa1c2465da7e18ee561fb4f6a3
Ilya Gavrilin <89260891+Ilyagavrilin@users.noreply.github.com>
2022-05-16 00:45:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 int main(){
   srand(time(0));
+  input_t* data;
   while ((data = InputScan()) != NULL){
     if (InputChkOccur(data)){
       printf("%ld\n", appendArrayTo2Q(data->requests, data->requests_sz, data->cache_size));

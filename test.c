926ae418ce64613a1d15d6ba8157c5d1cb8f7dfd
AntonDyacenko <59117867+AntonDyacenko@users.noreply.github.com>
2022-05-15 15:11:15 +0300

    Add files via upload

--- test.c
+++ test.c
@@ -9,8 +9,85 @@ int createN(int a){               // генератор N
     return n;
 }
 
-int main() {
+
+int bebra(unsigned long cache_sz, unsigned long arr_sz, unsigned long *arr) {
+    return 1;
+}
+
+void CreateArr(unsigned long a[], unsigned long size, unsigned long n_requests) {                  // генератор Массивов
+    for (unsigned long i = 0; i < size; i++) {
+        a[i] = rand();
+    }
+}
+
+int RunTests(int n_tests) {
+    unsigned long cache_sz = 0;
+    unsigned long arr_sz = 0;
+    unsigned long mass = 0;
+    unsigned long *arr;
+    int exArr_sz[5] = {5,
+                       5,
+                       5,
+                       5,
+                       5};
+
+    int exCache_sz[5] = {0,
+                         1,
+                         5,
+                         2,
+                         5};
+
+    int exArr[5][5] = {{1, 2, 3, 4, 5},
+                       {1, 2, 3, 4, 5},
+                       {1, 2, 3, 4, 5},
+                       {1, 2, 3, 4, 5},
+                       {1, 2, 3, 4, 5}};
+
+
+    for (int i = 0; i < n_tests; i++) {
+        if (i < 5){
+
+            arr_sz = exArr_sz[i];
+            cache_sz = exCache_sz[i];
+
+            mass = bebra(exArr_sz[i], exCache_sz[i], exArr[i]);
+
+            if (0 > mass || mass > arr_sz) {
+                return 0;
+            } else {
+                for (int j = 0; j < arr_sz; j++) {
+                    printf("%d ", exArr[i][j]);
+                }
+                printf("\n");
+            }
+
+        } else {
+            arr_sz = createN(10);
+            cache_sz = createN(10);
+
+            arr = (unsigned long *) calloc(arr_sz, sizeof(unsigned long)); //no malloc chacking
+            assert(arr != NULL);
+
+            CreateArr(arr, arr_sz, 100);
+            mass = bebra(cache_sz, arr_sz, arr);
+
+            if (0 > mass || mass > arr_sz) {
+                free(arr);
+                return 0;
+            } else {
+                for (int j = 0; j < arr_sz; j++) {
+                    printf("%lu ", arr[j]);
+                }
+                printf("\n");
+                free(arr);
+            }
+        }
+    }
+    return 1;
+}
+
+/*int main() {
     srand(time(NULL));//будет в main
     printf("\n%d", RunTests(10));
     return 0;
-}
+}*/
--- test.h
+++ test.h
@@ -5,85 +5,25 @@
 
 
 //--- header
+
+///Generating a random number in the range from 0 to a
 int createN(int a);
-//----.c file
+/*!
+    a
+*/
 
-int bebra(unsigned long cache_sz, unsigned long arr_sz, unsigned long *arr) {
-    return 1;
-}
 
-//[0, n_requests]
-void CreateArr(unsigned long a[], unsigned long size, unsigned long n_requests) {                  // генератор Массивов
-    for (unsigned long i = 0; i < size; i++) {
-        a[i] = rand();
-    }
-}
+///the function for starting the program (hereinafter, instead of it, the function for starting the algorithm)
+int bebra(unsigned long cache_sz, unsigned long arr_sz, unsigned long *arr);
+/*!
+    cache_sz - cache size
+    arr_sz - array size
+    arr - array of requests
+*/
+
+///random number list generation function
+void CreateArr(unsigned long a[], unsigned long size, unsigned long n_requests);
 
 
 //изменить на int 1 - success, 0 -
-int RunTests(int n_tests) {
-    unsigned long cache_sz = 0;
-    unsigned long arr_sz = 0;
-    unsigned long mass = 0;
-    unsigned long *arr;
-    int exArr_sz[5] = {5,
-                  5,
-                  5,
-                  5,
-                  5};
-
-    int exCache_sz[5] = {0,
-                  -1,
-                  5,
-                  2,
-                  5};
-
-    int exArr[5][5] = {{1, 2, 3, 4, 5},
-                       {-1, -2, -3, -4, -5},
-                       {1, 2, 3, 4, 5},
-                       {1, 2, 3, 4, 5},
-                       {1, 2, 3, 4, 5}};
-
-
-    for (int i = 0; i < n_tests; i++) {
-        if (i < 5){
-
-            arr_sz = exArr_sz[i];
-            cache_sz = exCache_sz[i];
-
-            mass = bebra(exArr_sz[i], exCache_sz[i], exArr[i]);
-
-            if (0 > mass || mass > arr_sz) {
-                return 0;
-            } else {
-                for (int j = 0; j < arr_sz; j++) {
-                    printf("%d ", exArr[i][j]);
-                }
-                printf("\n");
-            }
-
-        } else {
-            arr_sz = createN(10);
-            cache_sz = createN(10);
-
-            arr = (unsigned long *) calloc(arr_sz, sizeof(unsigned long)); //no malloc chacking
-            assert(arr != NULL);
-
-            CreateArr(arr, arr_sz, 100);
-            mass = bebra(cache_sz, arr_sz, arr);
-
-            if (0 > mass || mass > arr_sz) {
-                free(arr);
-                return 0;
-            } else {
-                for (int j = 0; j < arr_sz; j++) {
-                    printf("%lu ", arr[j]);
-                }
-                printf("\n");
-                free(arr);
-            }
-        }
-    }
-    return 1;
-}
-
+int RunTests(int n_tests);
